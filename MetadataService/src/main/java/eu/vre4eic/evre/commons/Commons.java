13849ad4580e772193a3ccb0b4fa189419914b86
YannisMarketakis <ymark_em@yahoo.gr>
2017-01-30 12:46:04 +0200

    skeleton and some javadoc comments for Commons class (and package)

---
+++ MetadataService/src/main/java/eu/vre4eic/evre/commons/Commons.java
@@ -0,0 +1,11 @@
+package eu.vre4eic.evre.commons;
+
+/**commons package contains some commonly used functionalities that are shared among
+ * the different components/classes. The same is true for Commons class.
+ * 
+ * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
+ * //FORTH team to add their credits here
+ */
+public class Commons {
+
+}
