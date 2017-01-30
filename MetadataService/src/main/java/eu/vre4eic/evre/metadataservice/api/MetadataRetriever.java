13c05bc7b4e186c8f393fcae253eed4531b484df
YannisMarketakis <ymark_em@yahoo.gr>
2017-01-30 12:49:37 +0200

    added sample interface and class for the metadata catalogue service (to be changed in future)

---
+++ MetadataService/src/main/java/eu/vre4eic/evre/metadataservice/api/MetadataRetriever.java
@@ -0,0 +1,11 @@
+package eu.vre4eic.evre.metadataservice.api;
+
+/** DEV comment: to change the interface and the signatures based on the actual design 
+ * and functionality of the MetadataCatalogue service.
+ * 
+ * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
+ * //FORTH team to add their credits here
+ */
+public interface MetadataRetriever {
+
+}
---
+++ MetadataService/src/main/java/eu/vre4eic/evre/metadataservice/impl/MetadataRetrieverImpl.java
@@ -0,0 +1,13 @@
+package eu.vre4eic.evre.metadataservice.impl;
+
+import eu.vre4eic.evre.metadataservice.api.MetadataRetriever;
+
+/** DEV comment: to change the interface and the signatures based on the actual design 
+ * and functionality of the MetadataCatalogue service.
+ * 
+ * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
+ * //FORTH team to add their credits here
+ */
+public class MetadataRetrieverImpl implements MetadataRetriever{
+
+}
