9d9125b3769b3192cb3378b0dc53fd942c24fc97
YannisMarketakis <ymark_em@yahoo.gr>
2017-01-30 12:39:49 +0200

    added signatures for the methods of the interface

--- MetadataService/src/main/java/eu/vre4eic/evre/datamodelmapper/api/MappingManager.java
+++ MetadataService/src/main/java/eu/vre4eic/evre/datamodelmapper/api/MappingManager.java
@@ -4,21 +4,41 @@ package eu.vre4eic.evre.datamodelmapper.api;
  * The Model Mapper component is responsible for performing the required mappings
  * and storing the particular information (the mappings themselves) in its repository.
  * 
- * The ModelMapperData interface exposes the functionalities as regards the data transformation.
+ * The MappingManager interface contains all the functionalities as regards the manipulation of mappings.
  * 
  * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
  * //FORTH team to add their credits here
  */
-public interface ModelMapperData {
+public interface MappingManager {
 
-    /** This method takes as input the textual description of some data a query
-     * and a description of the target schema and is responsible for transforming 
-     * them with respect to the target schema. The method returns the textual 
-     * description of the transformed data.
+    /** The method takes as input the ID of a mapping and returns the textual
+     * representation of the mappings (i.e. as an X3ML file).
      * 
-     * @param originalData the raw data in their original form
-     * @param targetSchema the schema describing the target format of the given raw data
-     * @return the transformed data
+     * @param mappingID the identifier of the mapping
+     * @return the string representation of the mapping
      */
-    public String transformData(String originalData, String targetSchema);
+    public String getMapping(String mappingID);
+    
+    /** The method takes as input a textual representation of a mapping 
+     * (i.e. a X3ML file), and an ID and stores the mapping in the MappingManager catalogue.
+     * 
+     * @param mappingID the identifier of the mapping
+     * @param mappingExpression the string representation of the mapping
+     */
+    public void addMapping(String mappingID, String mappingExpression);
+    
+    /** The method takes as input a textual representation of a mapping 
+     * (i.e. a X3ML file), and an ID and updates an existing mapping in the MappingManager catalogue.
+     * 
+     * @param mappingID the identifier of the mapping
+     * @param mappingExpression the string representation of the mapping
+     */
+    public void updateMapping(String mappingID, String mappingExpression);
+    
+    /** The method removes a mapping from the MappingManager catalogue using the 
+     * mapping identifier. 
+     * 
+     * @param mappingID the identifier of the mapping
+     */
+    public void deleteMapping(String mappingID);
 }
